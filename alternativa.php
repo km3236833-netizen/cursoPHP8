cea4559b24739b284b1698e3d748442ca4e519b7
km3236833-netizen <km3236833@gmail.com>
2026-02-09 08:32:44 -0600

    Create alternativa.php with PHP control structures
    Add PHP code examples for control structures.

---
+++ alternativa.php
@@ -0,0 +1,27 @@
+<?php 
+// viene una actualizacion 7.4
+$edad = 18;
+if($edad >= 18);
+echo "mayor de edad";
+endif;
+//semantico 
+
+if (condition):
+    # code...
+else:
+    # code...
+endif;
+
+while ();
+endwhile;
+
+for ($i=0; $i < ; $i++):
+    # code...
+endfor;
+
+foreach ();
+endforeach;
+
+//do while no tiene 
+
+?>
