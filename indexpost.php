db324e17f562b49c3e128afc8be39cb43a81fcf9
km3236833-netizen <km3236833@gmail.com>
2026-02-09 09:21:07 -0600

    Add indexpost.php for handling POST forms

---
+++ indexpost.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1> Procesar formularios POST </h1>
+    <form action="recibir.php" method="post">
+        <label form="" >Escribe tu nombre</label>
+        <br>
+        <input type="text" name="nombre" required>
+         <br>
+        <label form="" >Escribe tu materia</label>
+        <br>
+        <input type="text" name="materia">
+         <br>
+         
+        <button>enviar</button>
+    </form>
+</body>
+</html>
