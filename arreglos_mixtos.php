fcfa280fd980b1ac098a2c2169b9d0573f629e03
km3236833-netizen <km3236833@gmail.com>
2026-02-09 08:38:32 -0600

    Add mixed array example in arreglos_mixtos.php

---
+++ arreglos_mixtos.php
@@ -0,0 +1,17 @@
+<?php 
+/**
+ * arreglos mixtos 
+ */
+
+$arreglomixto =[
+    "curso" => "php 8",
+    0 => "facultad autodidacta",
+    "tipo" => "backend",
+    1 => "progra web"
+];
+//acceder 
+echo $arreglomixto["curso"];
+echo "<hr>";
+echo $arreglomixto[1];
+
+?>
