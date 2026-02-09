9af6971649f8a35e0af51372c8d6904e74593b8d
km3236833-netizen <km3236833@gmail.com>
2026-02-09 09:19:43 -0600

    Add recibirget.php to handle GET requests

---
+++ recibirget.php
@@ -0,0 +1,14 @@
+<?php
+//arreglo asiciativo nombre es una llave 
+var_dump($_GET);
+echo "<br>";
+$nombre =$_GET["nombre"];
+echo $nombre;
+$edad =$_GET["edad"];
+echo $edad;
+
+//emCapsular
+// procesar informacion
+//action que se vaya donde queremos 
+//cada uno de controles haran un arreglo que haran una funcion
+?>
