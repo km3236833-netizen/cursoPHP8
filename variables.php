235867c201c1d1a00aeb2aefee1820ba2daa46db
km3236833-netizen <km3236833@gmail.com>
2026-02-09 09:13:05 -0600

    Add variable declarations and rules in PHP

---
+++ variables.php
@@ -0,0 +1,31 @@
+<?php
+//vaariable: es un espacio de memoria reservo paras algun dato o informacion 
+
+/**
+ * Reglas para declarar variables 
+ * no se inician con numeros 
+ * no se declaran variables repetidas 
+ * no llevan espacio ni caracteres espciales
+ * no se usan palabras reservadas 
+ * por convencion se usa el camel case para declarar
+ */
+
+// es de tipado debil no lleva tipo de dato 
+// tipado estricto que viene de php 7 lo infiere intiede el tipado la informacion que le envies.
+//Variables numericas 
+
+$mivartiable = 5;
+
+//variables con punto fotante o decimal
+$miflotante= 8.9;
+
+//variables de string de cadena 
+$mistring ="hola que tal";
+$mistring2 = 'hola mundo ';
+
+//variables de tipo boolean
+$mibooleano = true;
+$mibooleano2 = false;
+$mibooleano3 = 1; //verdadero
+$mibooleano = 0; //falso
+?>
