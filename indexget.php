09cde4a50590203a613fc8e9f0f5d72e8cccaa36
km3236833-netizen <km3236833@gmail.com>
2026-02-09 09:17:30 -0600

    Add indexget.php for form processing

---
+++ indexget.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1> Procesar formularios </h1>
+    <form action="recibir.php" method="get">
+        <label form="" >Escribe tu nombre</label>
+        <br>
+        <input type="text" name="nombre">
+         <br>
+         <label form="" >Escribe tu edad</label>
+        <br>
+        <input type="text" name="edad">
+         <br>
+         
+        <button>enviar</button>
+    </form>
+</body>
+</html>
