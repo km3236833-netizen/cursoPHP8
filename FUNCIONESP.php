8b6812bc330ef0cd7edf6714989c88e7785349c6
km3236833-netizen <km3236833@gmail.com>
2026-02-09 09:02:03 -0600

    Add PHP functions for greeting and course display

---
+++ FUNCIONESP.php
@@ -0,0 +1,16 @@
+<?php 
+// funciones 
+// es un bloque de codigo reutilizable 
+
+function saludar (){
+    echo "hola";
+}
+
+saludar();
+saludar();
+//funciones sin parametro de retorno 
+function mostrarcurso(){
+    echo "este es el curso de php 8";
+}
+mostrarcurso();
+?>
