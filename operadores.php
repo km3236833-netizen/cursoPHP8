f6d972ab55f5529ac4875c09c9ddb200a6e8505a
km3236833-netizen <km3236833@gmail.com>
2026-02-09 09:07:22 -0600

    Create operadores.php with arithmetic examples
    Add PHP script demonstrating arithmetic operators and concatenation.

---
+++ operadores.php
@@ -0,0 +1,28 @@
+<?php 
+//operadores aritmeticos
+/**
+ * +  -   *     /  %   **  .
+ * suma resta multiplicacion division potencia concatenacion
+ * 
+ * 
+ */
+
+$a = 10;
+$b = 3;
+echo $a + $b;
+echo "<hr>";
+echo $a - $b;
+echo "<hr>";
+echo $a * $b;
+echo "<hr>";
+echo $a / $b;
+echo "<hr>";
+echo $a % $b;
+echo "<hr>";
+echo $a ** $b;
+echo "<hr>";
+//concatenacion 
+$nombre = "karen";
+$saludo = "hola";
+echo $saludo    . $nombre;
+?>
