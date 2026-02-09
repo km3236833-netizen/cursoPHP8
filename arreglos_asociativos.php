07ad61771d0defc1f806bb81729ec25df92d1aa8
km3236833-netizen <km3236833@gmail.com>
2026-02-09 08:35:28 -0600

    Create arreglos_asociativos.php with associative array
    Fix variable reference in integer check and add missing echo statement.

---
+++ arreglos_asociativos.php
@@ -0,0 +1,20 @@
+<?php 
+$persona =[
+    "nombre" => "roldan",
+    "edad"   => 35,
+    "ciudad" => "Mexico",
+    "profesion" => "ing"
+];
+
+foreach ($persona as $key => $value) {
+    if(is_int(value)){
+        echo "el entero es $value";
+    }
+     echo "<br>";
+
+}
+//procesar informacion
+//checar
+
+?>
+
