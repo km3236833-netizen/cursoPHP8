2736201079f94b2a11f2724ee0705683f8f3e1ac
km3236833-netizen <km3236833@gmail.com>
2026-02-09 08:41:09 -0600

    Create comentarios.php with documentation comments
    Add PHP comments for documentation and explanation

---
+++ comentarios.php
@@ -0,0 +1,11 @@
+<?php
+//comentarios para docummentar
+// es un comentario de linia no se ejecutan en el servixdor 
+// sirven para el programador 
+# comentario de linea 
+/**
+ * comentario de varias lineas 
+ */
+
+//pone un html basica cuando no se viazualiza nada pero una intrisica
+?>
