fbd9eab6c78f1f2d158a5fdde19e2de9072485ed
km3236833-netizen <km3236833@gmail.com>
2026-02-09 08:43:13 -0600

    Add function to sum two parameters
    Define a function to sum two values and call it twice.

---
+++ funcion_parametros.php
@@ -0,0 +1,11 @@
+<?php 
+//funsion con perimetros 
+//parametro es mas que una variable dentro de una funcion 
+function sumar($valor,$valor2){
+    echo $valor + $valor2;
+}
+// permite que varias veces se ocupe ejemplo logueamos en faceboor una funcion para millones de usuarios.
+sumar(5,8);
+echo "<br>";
+sumar(10,9);
+?>
