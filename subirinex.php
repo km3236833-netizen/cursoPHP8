4505c2ae96cfd206469ed1ffdb8c626e5f7da2a0
km3236833-netizen <km3236833@gmail.com>
2026-02-09 09:43:19 -0600

    Create subirinex.php

---
+++ subirinex.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Subir archivos</title>
+</head>
+<body>
+    <h1>Subir un archivo al servidor con php</h1>
+    <form action="subir.php" method="post" enctype="multipart/form-data">
+    <input type="file" name="archivo">
+    <br>
+    <button>Subir archivo</button>
+    </form>
+</body>
+</html>
