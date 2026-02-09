7d5623a3abd5f493d1f83c5f13a2ebce0620ea96
km3236833-netizen <km3236833@gmail.com>
2026-02-08 21:29:00 -0600

    Create for.php with loop examples
    Add a PHP script that demonstrates for loop usage and multiplication table.

---
+++ for.php
@@ -0,0 +1,15 @@
+<?php
+// es una estructura de buble
+// esta tiene un inicio y un incremento  
+
+for ($i=1; $i < 10; $i++) { 
+    echo $i ."<br>";
+}
+echo "<hr>";
+// tabal de multiplicar 
+$numero =2;
+for ($i=0; $i < 10; $i++) { 
+    $tabla = $numero . " x ". $i . " = ". ($numero * $i);
+    echo $tabla . "<br>";
+}
+?>
