c5b706a93a467cde648c3a6091fe211307c17793
km3236833-netizen <km3236833@gmail.com>
2026-02-09 09:04:23 -0600

    Add PHP script to determine day of the week

---
+++ if_else_if.php
@@ -0,0 +1,21 @@
+<?php 
+// determinar el dia de la semana segun un numero 
+$dianumero =3;
+ if( $dianumero ==1){
+     echo "lunes";
+ } else if ($dianumero ==2){
+    echo "martes";
+ } else if ($dianumero ==3){
+     echo "miercoles";
+ } else if ($dianumero ==4){
+    echo "jueves";
+ } else if ($dianumero ==5){
+    echo "viernes";
+ } else if ($dianumero ==6){
+    echo "sabado";
+ } else if ($dianumero ==7){
+    echo "domingo";
+ }else {
+     echo "dia equivocado ";
+ }
+?>
